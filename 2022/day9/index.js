a779ea3b65421c7b67841225e43aa57ef43e46ad
sanishchirayath1 <sanishchirayath@gmail.com>
2022-12-09 23:01:18 +0530

    feat: day9 part1

---
+++ 2022/day9/index.js
@@ -0,0 +1,151 @@
+import { syncReadFile } from "../utils.js";
+
+// R 4
+// U 4
+// L 3
+// D 1
+// R 4
+// D 1
+// L 5
+// R 2
+
+let arr = syncReadFile("./input.txt");
+// sample.txt expects 13 as answer for part 1
+// sample.txt expects 36 as answer for part 2
+
+arr = arr.map((item) => {
+  return item.split(" ");
+});
+
+let maxX = 0;
+let maxY = 0;
+let minX = Infinity;
+let minY = Infinity;
+let startXY = [0, 0];
+
+arr.forEach((item) => {
+  let [direction, distance] = item;
+
+  switch (direction) {
+    case "R":
+      startXY[0] += parseInt(distance);
+      maxX = Math.max(maxX, startXY[0]);
+      minX = Math.min(minX, startXY[0]);
+      break;
+    case "U":
+      startXY[1] += parseInt(distance);
+      maxY = Math.max(maxY, startXY[1]);
+      minY = Math.min(minY, startXY[1]);
+      break;
+    case "L":
+      startXY[0] -= parseInt(distance);
+      maxX = Math.max(maxX, startXY[0]);
+      minX = Math.min(minX, startXY[0]);
+      break;
+    case "D":
+      startXY[1] -= parseInt(distance);
+      maxY = Math.max(maxY, startXY[1]);
+      minY = Math.min(minY, startXY[1]);
+      break;
+  }
+});
+
+console.log("maxX : ", maxX);
+console.log("maxY : ", maxY);
+
+let grid = new Array(maxY + Math.abs(minY) + 1).fill(0).map(() => {
+  return new Array(maxX + Math.abs(minX) + 1).fill(0);
+});
+
+console.log("grid : ", grid);
+
+startXY = [Math.abs(minX), Math.abs(minY)];
+let positionHeadXY = [...startXY];
+let positionTailXY = [...startXY];
+
+arr.forEach((item) => {
+  let [direction, distance] = item;
+
+  switch (direction) {
+    case "R":
+      for (let i = 0; i < parseInt(distance); i++) {
+        positionHeadXY[0]++;
+        handleTailMovement();
+      }
+      break;
+    case "U":
+      for (let i = 0; i < parseInt(distance); i++) {
+        positionHeadXY[1]++;
+        handleTailMovement();
+      }
+      break;
+    case "L":
+      for (let i = 0; i < parseInt(distance); i++) {
+        positionHeadXY[0]--;
+        handleTailMovement();
+      }
+      break;
+    case "D":
+      for (let i = 0; i < parseInt(distance); i++) {
+        positionHeadXY[1]--;
+        handleTailMovement();
+      }
+      break;
+  }
+});
+
+function handleTailMovement() {
+  let [headX, headY] = positionHeadXY;
+  let [tailX, tailY] = positionTailXY;
+
+  const goRight = headX > tailX && Math.abs(headX - tailX) > 1;
+  const goLeft = headX < tailX && Math.abs(headX - tailX) > 1;
+  const goUp = headY > tailY && Math.abs(headY - tailY) > 1;
+  const goDown = headY < tailY && Math.abs(headY - tailY) > 1;
+
+  const goRightUp =
+    (goRight && goUp) || (goUp && headX > tailX) || (goRight && headY > tailY);
+  const goRightDown =
+    (goRight && goDown) ||
+    (goDown && headX > tailX) ||
+    (goRight && headY < tailY);
+  const goLeftUp =
+    (goLeft && goUp) || (goUp && headX < tailX) || (goLeft && headY > tailY);
+  const goLeftDown =
+    (goLeft && goDown) ||
+    (goDown && headX < tailX) ||
+    (goLeft && headY < tailY);
+
+  if (goRightUp) {
+    tailX++;
+    tailY++;
+  } else if (goRightDown) {
+    tailX++;
+    tailY--;
+  } else if (goLeftUp) {
+    tailX--;
+    tailY++;
+  } else if (goLeftDown) {
+    tailX--;
+    tailY--;
+  } else if (goRight) {
+    tailX++;
+  } else if (goLeft) {
+    tailX--;
+  } else if (goUp) {
+    tailY++;
+  } else if (goDown) {
+    tailY--;
+  }
+  positionTailXY = [tailX, tailY];
+  console.log("positionTailXY : ", positionTailXY);
+  console.log("positionHeadXY : ", positionHeadXY);
+
+  grid[tailY][tailX] = 1;
+}
+
+console.log("grid : ", grid);
+
+let visited = grid.flat().filter((item) => item === 1).length;
+
+console.log("part1 >> ", visited);
