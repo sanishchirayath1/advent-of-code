b2c86e6f60236e0f3d34d38fc53fd3a09fef2a5b
sanish chirayath <sanishchirayath@gmail.com>
2023-12-07 15:02:24 +0530

    feat: day7

---
+++ 2023/day7/part1.js
@@ -0,0 +1,59 @@
+const determineHandValue = (hand) => {
+  const handSet = getHandSet(hand);
+
+  let hasPair = false;
+  let hasThreeOfAKind = false;
+  for (let key in handSet) {
+    const val = handSet[key];
+    if (val === 5) {
+      return 7; // Five of a kind
+    } else if (val === 4) {
+      return 6; // Four of a kind
+    } else if ((val === 3 && hasPair) || (val === 2 && hasThreeOfAKind)) {
+      return 5; // Full house
+    } else if (val === 2 && hasPair) {
+      return 3; // Two pair
+    } else if (val === 3) {
+      hasThreeOfAKind = true;
+    } else if (val === 2) {
+      hasPair = true;
+    }
+  }
+
+  if (hasThreeOfAKind) return 4;
+  if (hasPair) return 2;
+  return 1; // high card
+};
+
+const getHandSet = (hand) => {
+  const handSet = {};
+  for (let i = 0; i < hand.length; i++) {
+    handSet[hand[i]] = handSet[hand[i]] || 0;
+    handSet[hand[i]] += 1;
+  }
+  return handSet;
+};
+
+const faceCardVals = {
+  T: 10,
+  J: 11,
+  Q: 12,
+  K: 13,
+  A: 14,
+};
+
+const determineTieBreaker = (handA, handB) => {
+  for (let i = 0; i < handA.length; i++) {
+    const cardA = handA[i];
+    const cardB = handB[i];
+    const handAVal = isNaN(+cardA) ? faceCardVals[cardA] : +cardA;
+    const handBVal = isNaN(+cardB) ? faceCardVals[cardB] : +cardB;
+
+    if (handAVal > handBVal) return -1;
+    if (handBVal > handAVal) return 1;
+  }
+
+  return 0;
+};
+
+console.log(part1());
